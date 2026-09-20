72f39f1d01e39ccbee5805c26500dd8b933f0d7e
shravan-9594 <shravanwagh90@gmail.com>
2026-09-20 15:55:51 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="study-spin-v1"; const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))})
